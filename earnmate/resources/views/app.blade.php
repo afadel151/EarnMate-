71e7cb6660e525741a54ec06a555e807c9e24580
afadel151 <af8356207@gmail.com>
2024-12-11 08:32:46 +0100

    Added a favicon

--- earnmate/resources/views/app.blade.php
+++ earnmate/resources/views/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {{-- <link rel="icon" href="{{@vite('resources/views/logo-small.svg')}}"> --}}
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        <title inertia>{{ config('app.name', 'Toro') }}</title>
+        <link rel="shortcut icon" type="image/x-icon"  href="./logo-small.png">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
